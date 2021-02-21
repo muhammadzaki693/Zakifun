ae7a582c5897e6c1c4a27df496b16659f50b6d68
Muhammadzakirha <rzaki9353@gmail.com>
2021-02-21 10:50:02 +0000

    A

--- Zakifun/__init__.py
+++ Zakifun/__init__.py
@@ -1,2 +1,22 @@
-def test():
-  print("iam online now")
+import xlrd
+
+def create(file_path):
+    try:
+        wb = xlrd.open_workbook(file_path)
+        sheet = wb.sheet_by_index(0)
+    except Exception:
+        return "Error: Cannot read the excel. Please make sure you have entered the correct path."
+
+    response = []
+    for i in range(1, sheet.nrows):
+        value = {}
+        for j in range(0,sheet.ncols):
+            temp = {}
+            try:
+                data = str(sheet.cell_value(i, j))
+                temp[sheet.cell_value(0, j)] = data
+                value.update(temp)
+            except Exception:
+                pass
+        response.append(value)
+    return response
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+import setuptools
 from setuptools import setup
 
 setup(
     name='Zakifun',
     version='1.0.0',
     description='zakifun for you',
-    url='git@github.com:muhammadzaki693/Zakifun.git',
+    url='https://github.com/muhammadzaki693/Zakifun.git',
     author='muhammadzaki693',
-    author_email='rzaki9353@gmail.com'
+    author_email='rzaki9353@gmail.com',
     license='unlicense',
     packages=['Zakifun'],
     zip_safe=False
