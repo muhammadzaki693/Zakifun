7ab038d76866235521bdbcf5b12de884869a23db
Muhammadzakirha <rzaki9353@gmail.com>
2021-02-21 09:24:37 +0000

    Initial commit

---
+++ Zakifun/__init__.py
@@ -0,0 +1,2 @@
+def test():
+  print("iam online now")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Zakifun',
+    version='1.0.0',
+    description='zakifun for you',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='muhammadzaki693',
+    author_email='rzaki9353@gmail.com'
+    license='unlicense',
+    packages=['Zakifun'],
+    zip_safe=False
+)
