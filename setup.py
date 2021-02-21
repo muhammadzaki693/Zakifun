392833be2e4efe2e2e9f68cf74c5e95912d062ea
Muhammadzakirha <rzaki9353@gmail.com>
2021-02-21 09:30:18 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Zakifun',
     version='1.0.0',
     description='zakifun for you',
-    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    url='git@github.com:muhammadzaki693/Zakifun.git',
     author='muhammadzaki693',
     author_email='rzaki9353@gmail.com'
     license='unlicense',
